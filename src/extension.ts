de0464095ef3cbde06ffed490a51c4fe4c1826d1
Brendan Desmond <brendes@gmail.com>
2024-12-16 00:23:12 -0500

    update license

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,3 @@
-/*
- * Theme Font Switcher for VS Code
- * Copyright (C) [2024] - Theme Font Switcher Contributors
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
-
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
